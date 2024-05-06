9f01415c3dfe99237c3289ecbc374cc5e332b8a9
Sarah Guindy <sarahtguindy@gmail.com>
2024-05-06 11:25:48 -0400

    Add error handling to update-packages

--- commands/nuget_update_packages.py
+++ commands/nuget_update_packages.py
@@ -3,6 +3,7 @@ Updates all packages across projects to the latest version, or to the version sp
 """
 
 import os
+import sys
 import argparse
 import subprocess
 import xml.etree.ElementTree as ET
@@ -16,7 +17,7 @@ def is_package_installed(csproj_file: str, package_name: str) -> bool:
     tree = ET.parse(csproj_file)
     root = tree.getroot()
     package_references = root.findall(".//PackageReference")
-    installed_packages = [pkg.get("Include") for pkg in package_references]
+    installed_packages = [package.get("Include") for package in package_references]
 
     return package_name in installed_packages
 
@@ -43,19 +44,42 @@ def update_packages(args: argparse.Namespace) -> None:
 
     print("Updating packages...")
 
+    updated_count = 0
+
     for proj_dir in proj_dirs:
         csproj_file = os.path.join(proj_dir, os.path.basename(proj_dir) + ".csproj")
 
+        # Skip projects that don't already have the package installed
         if not is_package_installed(csproj_file, args.package):
             continue
 
-        # Redirect standard output and standard error
-        subprocess.run(
-            command,
-            cwd=proj_dir,
-            check=True,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
+        try:
+            # Redirect standard output and standard error
+            subprocess.run(
+                command,
+                cwd=proj_dir,
+                check=True,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
+
+            updated_count += 1
+
+        except subprocess.CalledProcessError:
+            print(f"Error updating '{args.package}' packages.")
+            sys.exit(1)
+
+    if not updated_count > 0:
+        print(f"No '{args.package}' packages found in '{args.solution}'.")
+        sys.exit(1)
+
+    if args.version:
+        print(
+            f"Updated {updated_count} '{args.package}' packages to version '{args.version}'."
+        )
+    else:
+        print(
+            f"Updated {updated_count} '{args.package}' packages to the latest version."
         )
 
-    print("Successfully updated packages.")
+    sys.exit(0)
